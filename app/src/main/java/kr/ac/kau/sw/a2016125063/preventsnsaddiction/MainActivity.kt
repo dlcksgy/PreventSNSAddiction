7ddbb01048b2c2c03818ae9065e2da3a27108caa
Factriot <dldmsgus2@naver.com>
2017-09-11 23:51:08 +0900

    일단 listView에 simple_list_item_1으로 채워둠. 차후에 커스텀 어뎁터로 변경계획.

--- app/src/main/java/kr/ac/kau/sw/a2016125063/preventsnsaddiction/MainActivity.kt
+++ app/src/main/java/kr/ac/kau/sw/a2016125063/preventsnsaddiction/MainActivity.kt
@@ -1,12 +1,45 @@
 package kr.ac.kau.sw.a2016125063.preventsnsaddiction
 
+import android.content.Intent
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val optionButton = findViewById(R.id.option_button) as Button
+        optionButton.setOnClickListener{
+            val intent = Intent(this@MainActivity, OptionActivity::class.java)
+            startActivity(intent)
+        }
+
+        //출처
+        //http://blog.naver.com/pluulove84/100153350054
+        var appList = ArrayList<String>()
+        var pm: PackageManager = getPackageManager()
+        var packs = pm.getInstalledApplications(PackageManager.GET_DISABLED_COMPONENTS)
+        //var appInfo: ApplicationInfo? = null
+
+        for(app:ApplicationInfo in packs){
+            //appInfo = ApplicationInfo()
+            //appInfo.loadIcon(pm)
+            //appInfo.mAppName = app.loadLabel(pm)
+            //appInfo.packageName
+            Log.d("onCreate",app.packageName.toString())
+            appList.add(app.loadLabel(pm).toString())
+        }
+
+        var list: ListView = findViewById(R.id.app_list) as ListView
+        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,appList)
+        list.setAdapter(adapter)
     }
 }
